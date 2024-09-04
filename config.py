cbe2f9794d14ef35a5194e0856be936f58518151
Mark :) <mark.sid2011@gmail.com>
2024-09-04 11:04:33 +0300

    Update config.py
    adding comments in a config file

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
-TITLE = "СryptoPriceTracker"
-GEOMETRY = "700x500"
-CRYPTOCURRENCY = ['BTC', 'ETH', 'BNB', 'DOGE', 'PEPE', 'SOL', 'TON']
+TITLE = "СryptoPriceTracker" # title of window
+GEOMETRY = "700x500" # geometry of window
+CRYPTOCURRENCY = ['BTC', 'ETH', 'BNB', 'DOGE', 'PEPE', 'SOL', 'TON'] # list of cryptocurrencies
 TIME_TO_SLEEP = 10000 # 10 seconds
-FONT = ("Arial", 35)
+FONT = ("Arial", 35) # font 
